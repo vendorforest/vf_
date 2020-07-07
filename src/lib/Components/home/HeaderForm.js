f679ab81079c53fe07ea3cd995bc9f061976fba9
vendorforest <56562380+vendorforest@users.noreply.github.com>
2020-07-07 17:48:58 -0400

    Update HeaderForm.js

--- src/lib/Components/home/HeaderForm.js
+++ src/lib/Components/home/HeaderForm.js
@@ -24,7 +24,7 @@ class HeaderForm extends React.Component {
 						<div className="col-12 d-flex align-items-center h-100">
 							<div className="w-100">
 								<h1 className="mb-5">The one place to hire the right vendor for anything.</h1>
-								<h3 className="mb-4">Find local professionals specialized with the right skills ready to work because your satisfaction is our reward.</h3>
+								<h3 className="mb-4">Find local vendors specialized with the right skills ready to work because your satisfaction is our reward.</h3>
 								<Button type="primary" style={{minWidth: '150px'}} size="large">Post a Job</Button>
 							</div>
 						</div>
